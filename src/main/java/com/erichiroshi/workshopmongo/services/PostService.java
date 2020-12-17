15835893b555d92aefb3f4492c3a0da70b4d6c00
erichiroshi <erichiroshi@gmail.com>
2020-12-17 17:59:17 -0400

    Consulta simples com query methods

--- src/main/java/com/erichiroshi/workshopmongo/services/PostService.java
+++ src/main/java/com/erichiroshi/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.erichiroshi.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
+	
 }
