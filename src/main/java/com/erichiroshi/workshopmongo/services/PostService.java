1075c29dadccde0e4753203cdbcf90f0549ac948
erichiroshi <erichiroshi@gmail.com>
2020-12-17 18:31:40 -0400

    Consulta com vários critérios

--- src/main/java/com/erichiroshi/workshopmongo/services/PostService.java
+++ src/main/java/com/erichiroshi/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.erichiroshi.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,8 +22,13 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
-	public List<Post> findByTitle(String text){
+	public List<Post> findByTitle(String text) {
 		return repository.searchTitle(text);
 	}
-	
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repository.fullSearch(text, minDate, maxDate);
+	}
+
 }
