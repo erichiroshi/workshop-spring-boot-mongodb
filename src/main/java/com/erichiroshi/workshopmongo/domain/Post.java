eb98d8b250816434dbcc497bd141735a4285cc59
erichiroshi <erichiroshi@gmail.com>
2020-12-17 16:35:56 -0400

    Criando entity Post com User aninhado

---
+++ src/main/java/com/erichiroshi/workshopmongo/domain/Post.java
@@ -0,0 +1,33 @@
+package com.erichiroshi.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Document(collection = "post")
+public class Post implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EqualsAndHashCode.Include
+	@Id
+	private String id;
+
+	private Date date;
+	private String title;
+	private String body;
+	
+	private User author;
+}
