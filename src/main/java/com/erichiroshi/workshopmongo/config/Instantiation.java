4f4d068bf62a64be39e93edf132b6cfeb7b77fdb
erichiroshi <erichiroshi@gmail.com>
2020-12-17 16:36:09 -0400

    Projeção dos dados do autor com DTO

--- src/main/java/com/erichiroshi/workshopmongo/config/Instantiation.java
+++ src/main/java/com/erichiroshi/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.erichiroshi.workshopmongo.domain.Post;
 import com.erichiroshi.workshopmongo.domain.User;
+import com.erichiroshi.workshopmongo.dto.AuthorDTO;
 import com.erichiroshi.workshopmongo.repositories.PostRepository;
 import com.erichiroshi.workshopmongo.repositories.UserRepository;
 
@@ -35,8 +36,8 @@ public class Instantiation implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
--- src/main/java/com/erichiroshi/workshopmongo/domain/Post.java
+++ src/main/java/com/erichiroshi/workshopmongo/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.erichiroshi.workshopmongo.dto.AuthorDTO;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -28,6 +30,6 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	
-	private User author;
+
+	private AuthorDTO author;
 }
