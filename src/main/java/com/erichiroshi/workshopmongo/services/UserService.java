1426068f1cea8de283d025ce9bd67e444de997b2
erichiroshi <erichiroshi@gmail.com>
2020-12-17 13:47:36 -0400

    Obtendo um usuário por id

--- src/main/java/com/erichiroshi/workshopmongo/services/UserService.java
+++ src/main/java/com/erichiroshi/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.erichiroshi.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.erichiroshi.workshopmongo.domain.User;
 import com.erichiroshi.workshopmongo.repositories.UserRepository;
+import com.erichiroshi.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
