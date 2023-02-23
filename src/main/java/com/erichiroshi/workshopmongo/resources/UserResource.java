efe3b4b7567a7c9db4610a6236c0f47d6ad70dff
erichiroshi <erichiroshi@gmail.com>
2023-02-23 13:45:49 -0400

    Deleção de usuário com DELETE

--- src/main/java/com/erichiroshi/workshopmongo/resources/UserResource.java
+++ src/main/java/com/erichiroshi/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,11 @@ public class UserResource {
 		userDTO = new UserDTO(user);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).body(userDTO);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
