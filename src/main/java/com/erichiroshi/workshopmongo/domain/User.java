e7edfeddf9b2e36cb863c5218c6e48f93f608f7e
erichiroshi <erichiroshi@gmail.com>
2023-02-23 15:41:43 -0400

    Referenciando os posts do usuário

--- src/main/java/com/erichiroshi/workshopmongo/domain/User.java
+++ src/main/java/com/erichiroshi/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.erichiroshi.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -20,6 +23,9 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	public User(String id, String name, String email) {
 		this.id = id;
 		this.name = name;
